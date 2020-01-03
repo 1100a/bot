a54665a12e5d8a80b007257a80ff44caf5419eab
안정언 <powerserver9011@gmail.com>
2020-01-03 23:50:35 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,28 @@
+import asyncio
 import discord
-import os
-
 
 client = discord.Client()
 
+# 생성된 토큰을 입력해준다.
+token = "토큰"
 
+# 봇이 구동되었을 때 보여지는 코드
 @client.event
 async def on_ready():
-    print("login")
+    print("다음으로 로그인합니다")
     print(client.user.name)
     print(client.user.id)
-    print("------")
-    await client.change_presence(game=discord.Game(name='', type=1))
-    
+    print("================")
+
+# 봇이 특정 메세지를 받고 인식하는 코드
 @client.event
 async def on_message(message):
-    if message.content.startswith("hi"):
-        await client.send_message(message.channel, "HI")
-        
-        
-access_token = os.environ["BOT_TOKEN"]
-client.run(access_token)
+    # 메세지를 보낸 사람이 봇일 경우 무시한다
+    if message.author.bot:
+        return None
+
+    if message.content.startswith('!안녕'):
+        channel = message.channel
+        await channel.send('반가워!')
+
+client.run(token)
